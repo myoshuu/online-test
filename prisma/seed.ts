a7e264f9cae969544e58677a99f5115d913dcc01
myoshuu <myoshu.me@gmail.com>
2025-11-25 21:36:05 +0700

    feat: make the import statically

--- prisma/seed.ts
+++ prisma/seed.ts
@@ -1,5 +1,5 @@
 import bcrypt from "bcryptjs";
-import { PrismaClient } from "@/app/generated/prisma/client";
+import { PrismaClient } from "../app/generated/prisma/client";
 import { PrismaPg } from "@prisma/adapter-pg";
 
 const prisma = new PrismaClient({
