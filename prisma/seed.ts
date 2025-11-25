d1db6931e79d8ba376d4c0934824a5e025fdb198
myoshuu <myoshu.me@gmail.com>
2025-11-26 01:42:26 +0700

    feat: the user data for initial database

--- prisma/seed.ts
+++ prisma/seed.ts
@@ -55,6 +55,83 @@ const main = async () => {
     },
   });
 
+  const studentData = [
+    {
+      name: "ABEL RAYJHON TASMAN SARAGIH",
+      nim: "2401001",
+      password: bcrypt.hashSync("2401001", 12),
+      role: "STUDENT" as const,
+      isActive: true,
+    },
+    {
+      name: "CINTYA JUNIARTI",
+      nim: "2401002",
+      password: bcrypt.hashSync("2401002", 12),
+      role: "STUDENT" as const,
+      isActive: true,
+    },
+    {
+      name: "ERI PERMATA SARI",
+      nim: "2401003",
+      password: bcrypt.hashSync("2401003", 12),
+      role: "STUDENT" as const,
+      isActive: true,
+    },
+    {
+      name: "MANDA",
+      nim: "2401004",
+      password: bcrypt.hashSync("2401004", 12),
+      role: "STUDENT" as const,
+      isActive: true,
+    },
+    {
+      name: "LATRI FUJA MARISA",
+      nim: "2401005",
+      password: bcrypt.hashSync("2401005", 12),
+      role: "STUDENT" as const,
+      isActive: true,
+    },
+    {
+      name: "VERDIAN IMANUEL",
+      nim: "2401008",
+      password: bcrypt.hashSync("2401008", 12),
+      role: "STUDENT" as const,
+      isActive: true,
+    },
+    {
+      name: "WINDA",
+      nim: "2401009",
+      password: bcrypt.hashSync("2401009", 12),
+      role: "STUDENT" as const,
+      isActive: true,
+    },
+    {
+      name: "YEREMIAS BAGAS",
+      nim: "2401010",
+      password: bcrypt.hashSync("2401010", 12),
+      role: "STUDENT" as const,
+      isActive: true,
+    },
+    {
+      name: "DANIEL REYNALDI ANDRIAS",
+      nim: "2501003",
+      password: bcrypt.hashSync("2501003", 12),
+      role: "STUDENT" as const,
+      isActive: true,
+    },
+    {
+      name: "HAFAAHAKHODODO GULO",
+      nim: "2501018",
+      password: bcrypt.hashSync("2501018", 12),
+      role: "STUDENT" as const,
+      isActive: true,
+    },
+  ];
+
+  await prisma.user.createMany({
+    data: studentData,
+  });
+
   console.log("✅ Seed completed successfully!");
 };
 
